0535bf10dbefab6f4102e763664775c016229a78
naufalhakim <hakimnaufal212@gmail.com>
2022-07-26 14:50:47 +0700

    Feat: Adding new popup for next feature description pokemon

---
+++ src/components/pokemon/QuickViewPoke.tsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+
+const QuickViewPoke = ({ url }: { url: string }) => {
+  const [pokemon, setPokemon] = React.useState<any>({});
+  // const [loading, setLoading] = React.useState<boolean>(true);
+  const fetchDescription = async (url: string) => {
+    try {
+      const response = await axios({
+        method: "GET",
+        url: url,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setPokemon(response.data);
+      // setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchDescription(url);
+  }, [url]);
+  return (
+    <div>
+      <div className="relative z-10" role="dialog" aria-modal="true">
+        <pre>{JSON.stringify(pokemon)}</pre>
+      </div>
+    </div>
+  );
+};
+
+export default QuickViewPoke;
