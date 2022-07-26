b3609738d30b6a0c765c9b22d0069a8ebe0ea1d0
naufalhakim <hakimnaufal212@gmail.com>
2022-07-26 14:53:20 +0700

    Add: QuickView to main pokemon page

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,14 +1,18 @@
-import { Flex, Grid, Box } from "@chakra-ui/react";
+import { Flex, Grid, Box, Container } from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect } from "react";
 import CardPoke from "../components/pokemon/CardPoke";
 import HeroPoke from "../components/pokemon/HeroPoke";
 import NavbarPoke from "../components/pokemon/NavbarPoke";
+import QuickViewPoke from "../components/pokemon/QuickViewPoke";
 
 const Pokemon = () => {
   const [post, setPost] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(true);
   const [isError, setIsError] = React.useState(false);
+  const [urlDesc, setUrlDesc] = React.useState(null);
+  const [isClicked, setIsClicked] = React.useState(false);
+  const [numberIndex, setNumberIndex] = React.useState(0); //still not getting from click event
   const fetchData = async () => {
     try {
       const res = await axios({
@@ -20,14 +24,15 @@ const Pokemon = () => {
       });
       setPost(res.data);
       setIsLoading(false);
+      setUrlDesc(res.data.results[numberIndex].url);
     } catch (error) {
       setIsError(true);
     }
   };
   useEffect(() => {
     fetchData();
-  }, []);
-  console.log(post);
+  }, [
+  ]);
   return (
     <div>
       <NavbarPoke />
@@ -39,30 +44,93 @@ const Pokemon = () => {
         mt={20}
       >
         <h1>Pokemon</h1>
-        {/* {JSON.stringify(post)} */}
       </Flex>
-      <Grid templateColumns="repeat(5, 1fr)">
-        {isLoading ? (
-          <Box>Loading...</Box>
-        ) : isError ? (
-          <Box>Error...</Box>
+      <Container
+        zIndex={10}
+      >
+
+        {isClicked ? (
+          <QuickViewPoke
+            url={urlDesc}
+          />
         ) : (
-          post &&
-          post.results?.map((item, index) => {
-            return (
-              <Box>
-                <CardPoke
-                  key={index}
-                  name={item.name}
-                  url={item.url}
-                  id={index + 1}
-                />
-              </Box>
-            );
-          })
-        )
-        }
-      </Grid>
+          <Grid
+            templateColumns="repeat(auto-fit, minmax(200px, 1fr))"
+            gap={4}
+            mt={4}
+            mb={4}
+          >
+          </Grid>
+        )}
+      </Container>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          flexWrap: "wrap",
+          marginTop: "20px",
+        }}
+      >
+        <Grid templateColumns="repeat(4, 1fr)"
+          gap={4}
+          mt={20}
+          mb={20}
+          p={4}
+          borderWidth="1px"
+          borderColor="gray.200"
+          borderStyle="solid"
+          borderRadius={4}
+          boxShadow="0px 0px 10px rgba(0, 0, 0, 0.1)"
+          overflow="hidden"
+          transition="all 0.2s"
+        >
+          {isLoading ? (
+            <Box
+              width="100%"
+              height="100%"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+
+            >Loading...</Box>
+          ) : isError ? (
+            <Box
+              width="100%"
+              height="100%"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >Error...</Box>
+          ) : (
+            post &&
+            post.results?.map((item, index) => {
+              return (
+                <>
+                  <Box
+                    onClick={() => setIsClicked(!isClicked)}
+                  >
+                    <CardPoke
+                      key={index}
+                      name={item.name}
+                      url={item.url}
+                      id={index + 1}
+                      onClick={() => {
+                        setIsClicked(true);
+                        setNumberIndex(index + 1);
+                      }}
+                    />
+                  </Box>
+                </>
+              );
+            })
+          )
+          }
+
+        </Grid>
+      </div
+      >
     </div>
   );
 };
