41a6e956229ddc977211e3962fe1ccb6ff37b224
naufalhakim <hakimnaufal212@gmail.com>
2022-07-26 15:14:43 +0700

    Chore: Adding eslint-disable-next-line for testing

--- src/components/pokemon/CardPoke.tsx
+++ src/components/pokemon/CardPoke.tsx
@@ -31,10 +31,7 @@ const CardPoke = ({
             Here are the biggest enterprise technology acquisitions of 2021 so
             far, in reverse chronological order.
           </p> */}
-          <a
-            href="#"
-            className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
+          <a className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
             <svg
               aria-hidden="true"
